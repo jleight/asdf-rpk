b4fe419ca3283e6d710bf8d6a6d6bba2e120e5bb
Jonathon Leight <github@jleight.com>
2024-08-16 08:20:46 -0400

    fix: grep for tags list (#6)
    * Fix workflow name
    * Fix shellcheck source
    * Fix grep

--- lib/utils.bash
+++ lib/utils.bash
@@ -24,7 +24,7 @@ sort_versions() {
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/v\d.*' |
+		grep 'refs/tags/v[[:digit:]]' |
 		cut -d/ -f3- |
 		sed 's/^v//'
 }
